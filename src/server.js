bfa293abfa8edd10174a14a1888ccd832ed02bfb
HenrithanN <jonathanhenrique1515@gmail.com>
2023-12-10 16:04:05 -0300

    removendo app js + adicionando biblioteca de cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-const app = require('./app');
-const router = require('./router');
+const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors')
+const router = require('./router');
 
-dotenv.config();
-
+app.use(cors());
+app.use(express.json());
+dotenv.config()
 const NODE_APP_PORT = process.env.port || 3000
 app.use(router);
 
